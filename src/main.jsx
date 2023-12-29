58d422b01a9517a5b7b8f54d904096ec5bf05cfe
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-12-29 18:35:04 -0500

    Add react router, change App.jsx to Index.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  RouterProvider,
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+} from 'react-router-dom';
+import Index from './components';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Index />}
+    />,
+  ),
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
