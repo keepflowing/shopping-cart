facdde813fc29681a0650744edf6007042dd92c7
keepflowing <132106900+keepflowing@users.noreply.github.com>
2024-01-09 18:21:35 -0500

    Create CartButton and add it to Navbar

---
+++ src/components/CartButton/CartButton.jsx
@@ -0,0 +1,8 @@
+import style from './CartButton.module.css'
+
+export default function CartButton() {
+  return (
+    <div className={style.button}>
+    </div>
+  )
+}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
 import style from './Navbar.module.css';
+import CartButton from '../CartButton/CartButton';
 
 export default function Navbar() {
   return (
     <nav className={style.navbar}>
-      <h1 className={style.title}>My store</h1>
-      <ul>
-        <li className={style.navItem}><Link to="/">Home</Link></li>
-        <li className={style.navItem}><Link to="/store">Store</Link></li>
-        <li className={style.navItem}><Link to="/contact">Contact</Link></li>
-      </ul>
+      <div className={style.left} >
+        <h1 className={style.title}>My store</h1>
+        <ul>
+          <li className={style.navItem}><Link to="/">Home</Link></li>
+          <li className={style.navItem}><Link to="/store">Store</Link></li>
+          <li className={style.navItem}><Link to="/contact">Contact</Link></li>
+        </ul>
+      </div>
+      <CartButton />
     </nav>
   );
 }
