82d06b69eceb704a6500f456d8836e991344c89d
keepflowing <132106900+keepflowing@users.noreply.github.com>
2024-01-10 14:57:49 -0500

    Add dropdown effect to CartButton

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -1,9 +1,18 @@
-import style from './CartButton.module.css'
+import style from './CartButton.module.css';
+import { useState } from 'react';
+import CartMenu from '../CartMenu/CartMenu';
 
 export default function CartButton({cart}) {
+  const [ open, setOpen ] = useState(false);
+
   return (
-    <div className={style.button}>
+    <div 
+      className={style.button}
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+    >
       {cart.length}
+      {open && <CartMenu onLeave={() => setOpen(false)}/>}
     </div>
   )
 }
---
+++ src/components/CartMenu/CartMenu.jsx
@@ -0,0 +1,9 @@
+import style from './CartMenu.module.css'
+
+export default function CartMenu() {
+  return (
+    <div className={style.menu}>
+      <p>CartMenu</p>
+    </div>
+  )
+}
