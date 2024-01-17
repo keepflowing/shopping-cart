f8bcf58321d4fca80c2342e1a855d5b30fade312
keepflowing <132106900+keepflowing@users.noreply.github.com>
2024-01-17 14:19:13 -0500

    Create standard directories, remove old code

--- src/__tests__/Errorpage.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Errorpage from '../components/Errorpage/Errorpage';
-
-describe('Errorpage', () => {
-  it('renders title', () => {
-    render(<Errorpage />);
-    expect(screen.getByRole('heading').textContent).toMatch(/404 - page not found/i);
-  });
-});
