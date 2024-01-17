1e0d7759e6265c6bd6f532b0e9f88cd1355794fa
keepflowing <132106900+keepflowing@users.noreply.github.com>
2024-01-17 15:21:52 -0500

    Re-create index and App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element="hello" />
+      </Routes>
+    </main>
+  );
+}
