6dbc9e1a4279c959d896236af825b4f19ad578c5
keepflowing <132106900+keepflowing@users.noreply.github.com>
2024-01-28 20:13:51 -0500

    Add icons to navbar, make it responsive to mobile screens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import { Link, NavLink } from 'react-router-dom';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { AiOutlineShopping, AiOutlineSearch } from 'react-icons/ai';
 import styles from './Navbar.module.css';
 
 export default function Navbar({ title }) {
@@ -7,30 +9,44 @@ export default function Navbar({ title }) {
       className={styles.navbar}
       data-testid="navbar"
     >
-      <div className={styles.left}>
-        <Link to="/">
-          <h2>{title}</h2>
-        </Link>
+      <div className={styles.first}>
+
+        <div className={styles.left}>
+          <Link to="/">
+            <h2>{title}</h2>
+          </Link>
+        </div>
+        <div className={styles.right}>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : undefined)}
+          >
+            <p>Home</p>
+          </NavLink>
+          <NavLink
+            to="/store"
+            className={({ isActive }) => (isActive ? styles.active : undefined)}
+          >
+            <p>Store</p>
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) => (isActive ? styles.active : undefined)}
+          >
+            <p>Contact</p>
+          </NavLink>
+        </div>
       </div>
-      <div className={styles.right}>
-        <NavLink
-          to="/"
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-        >
-          <p>Home</p>
-        </NavLink>
-        <NavLink
-          to="/store"
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-        >
-          <p>Store</p>
-        </NavLink>
-        <NavLink
-          to="/contact"
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-        >
-          <p>Contact</p>
-        </NavLink>
+      <div className={styles.icons}>
+        <div className={styles.search}>
+          <AiOutlineSearch />
+        </div>
+        <div className={styles.cart}>
+          <AiOutlineShopping />
+        </div>
+        <div className={styles.hamburger}>
+          <RxHamburgerMenu />
+        </div>
       </div>
     </header>
   );
